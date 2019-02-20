107aea6cb1cbd3e7c90c03a328e3677e4a90cba7
Jarret Rose <jarretrose@gmail.com>
2019-02-20 11:54:40 -0500

    added information about projects to modals

--- jsfile.js
+++ jsfile.js
@@ -1,19 +1,33 @@
-// PROJECT MODAL
-const modal = document.getElementById('project-modal');
-const span = document.getElementsByClassName('close')[0];
+// PROJECT MODAL 1 (code is sloppy and redundant here, need to refactor)
+const modal1 = document.getElementById('project-modal1');
+const span1 = document.getElementById('close1');
+const modalBtn1 = document.getElementById('modal-button1');
 
-const modalBtn = document.getElementById('modal-button');
+modalBtn1.onclick = () => modal1.style.display = 'block';
+span1.onclick = () => modal1.style.display = 'none';
+
+// PROJECT MODAL 2
+const modal2 = document.getElementById('project-modal2');
+const span2 = document.getElementById('close2');
 const modalBtn2 = document.getElementById('modal-button2');
+
+modalBtn2.onclick = () => modal2.style.display = 'block';
+span2.onclick = () => modal2.style.display = 'none';
+
+// PROJECT MODAL 2
+const modal3 = document.getElementById('project-modal3');
+const span3 = document.getElementById('close3');
 const modalBtn3 = document.getElementById('modal-button3');
 
-modalBtn.onclick = () => modal.style.display = 'block';
-modalBtn2.onclick = () => modal.style.display = 'block';
-modalBtn3.onclick = () => modal.style.display = 'block';
+modalBtn3.onclick = () => modal3.style.display = 'block';
+span3.onclick = () => modal3.style.display = 'none';
 
-span.onclick = () => modal.style.display = 'none';
-
-window.onclick = () => 
-  event.target == modal ? modal.style.display = 'none' : null
+// Click anywhere else to close the modal
+window.onclick = (event) => {
+  if (event.target == modal1) modal1.style.display = 'none'
+  if (event.target == modal2) modal2.style.display = 'none'
+  if (event.target == modal3) modal3.style.display = 'none'
+}
 
 
 // RETURN TO TOP BUTTON
@@ -24,7 +38,7 @@ const scrollFunction = () => {
   const element = document.documentElement.scrollTop
   const topBtn = document.getElementById("topButton")
 
-  if (body > 20 || element > 20) { topBtn.style.display = "block";} 
+  if (body > 20 || element > 20) { topBtn.style.display = "block"; }
   else { topBtn.style.display = "none"; }
 }
 
@@ -37,7 +51,7 @@ function topFunction() {
 const toggleEmail = () => {
   const emailer = document.getElementById('emailer')
   const connect = document.getElementById('connect')
-  
+
   if (emailer.style.display === 'none') {
     emailer.style.display = 'block'
     emailer.scrollIntoView()
