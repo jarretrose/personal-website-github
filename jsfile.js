e336f2aa78e8bbb82c5a80f6903ecba311f938a4
Jarret Rose <jarretrose@gmail.com>
2019-03-28 13:09:38 -0400

    whole site revamp

--- jsfile.js
+++ jsfile.js
@@ -1,34 +1,3 @@
-// PROJECT MODAL 1 (code is sloppy and redundant here, need to refactor)
-const modal1 = document.getElementById('project-modal1');
-const span1 = document.getElementById('close1');
-const modalBtn1 = document.getElementById('modal-button1');
-
-modalBtn1.onclick = () => modal1.style.display = 'block';
-span1.onclick = () => modal1.style.display = 'none';
-
-// PROJECT MODAL 2
-const modal2 = document.getElementById('project-modal2');
-const span2 = document.getElementById('close2');
-const modalBtn2 = document.getElementById('modal-button2');
-
-modalBtn2.onclick = () => modal2.style.display = 'block';
-span2.onclick = () => modal2.style.display = 'none';
-
-// PROJECT MODAL 2
-const modal3 = document.getElementById('project-modal3');
-const span3 = document.getElementById('close3');
-const modalBtn3 = document.getElementById('modal-button3');
-
-modalBtn3.onclick = () => modal3.style.display = 'block';
-span3.onclick = () => modal3.style.display = 'none';
-
-// Click anywhere else to close the modal
-window.onclick = (event) => {
-  if (event.target == modal1) modal1.style.display = 'none'
-  if (event.target == modal2) modal2.style.display = 'none'
-  if (event.target == modal3) modal3.style.display = 'none'
-}
-
 // RETURN TO TOP BUTTON
 window.onscroll = () => scrollFunction();
 
@@ -49,7 +18,7 @@ function topFunction() {
 /* HIDE / SHOW EMAIL FORM */
 const toggleEmail = () => {
   const emailer = document.getElementById('emailer')
-  const connect = document.getElementById('connect')
+  const connect = document.getElementById('_connect')
 
   if (emailer.style.display === 'none') {
     emailer.style.display = 'block'
