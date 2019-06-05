7fffd32671677013e9f1c9cd820127cfdbb86453
Jarret Rose <jarretrose@gmail.com>
2019-06-05 10:39:43 -0400

    more experimenting

--- jsfile2.js
+++ jsfile2.js
@@ -1,10 +1,10 @@
 window.onload=function() {
 	// Get the Object by ID
-	var a = document.getElementById("svgObject");
+	var a = document.getElementsByClassName("svg-view-box");
 	// Get the SVG document inside the Object tag
 	var svgDoc = a.contentDocument;
 	// Get one of the SVG items by ID;
-	var svgItem = svgDoc.getElementById("svgItem");
+	var svgItem = svgDoc.querySelectorAll("path");
 	// Set the colour to something else
 	svgItem.setAttribute("fill", "lime");
 };
