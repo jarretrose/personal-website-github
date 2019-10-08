99ea51783266d3bb2deef6e48b5f5ca8723b2749
Jarret Rose <jarretrose@gmail.com>
2019-10-07 23:20:38 -0400

    some updates

--- jsfile.js
+++ jsfile.js
@@ -28,21 +28,23 @@ const toggleEmail = () => {
     connect.scrollIntoView(false, {behavior: 'smooth'})
   }
 }
-// ********************
+
+// Filter / Show Skills
 filterSelection("all")
+
 function filterSelection(c) {
-  var x, i;
+  let x, i;
   x = document.getElementsByClassName("individual-skill");
   if (c == "all") c = "";
   // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
   for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+    removeClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) addClass(x[i], "show");
   }
 }
 
-// Show filtered elements
-function w3AddClass(element, name) {
+// Show filtered skills
+function addClass(element, name) {
   var i, arr1, arr2;
   arr1 = element.className.split(" ");
   arr2 = name.split(" ");
@@ -54,7 +56,7 @@ function w3AddClass(element, name) {
 }
 
 // Hide elements that are not selected
-function w3RemoveClass(element, name) {
+function removeClass(element, name) {
   var i, arr1, arr2;
   arr1 = element.className.split(" ");
   arr2 = name.split(" ");
@@ -67,8 +69,8 @@ function w3RemoveClass(element, name) {
 }
 
 // Add active class to the current control button (highlight it)
-var btnContainer = document.getElementById("myBtnContainer");
-var btns = btnContainer.getElementsByClassName("btn");
+var btnContainer = document.getElementById("skill-btns");
+var btns = btnContainer.getElementsByClassName("skill-btn");
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
     var current = document.getElementsByClassName("active");
